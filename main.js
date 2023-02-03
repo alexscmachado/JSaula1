1c050082d5e618eaba1d7ac8db583326bf45a079
Alex Machado <coderaxmcaps@gmail.com>
2023-02-02 21:45:06 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let nome1 = prompt ("digte seu primeiro nome:");
+let nome2 = prompt ("digite o seu sobrenome:")
+
+let nomeTodo = nome1+"  "+nome2
+
+alert ( "Seu nome é:" + nomeTodo)
+
+let birth = prompt ("digte a seu ano de nascimento");
+
+let idade = 2023 - birth;
+
+alert ("Sua idade é:" + " " + idade);
+
+
